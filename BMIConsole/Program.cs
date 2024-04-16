8ac59da7d2477624ad982eb891a488e5ed4b23c3
wawer <wawer@KIWI>
2024-04-16 21:40:34 +0200

    Add project files.

---
+++ BMIConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace BMIConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            BMICalculator.BMICalculator calc = new BMICalculator.BMICalculator();
+
+            Console.WriteLine(calc.Calculate(80.0, 180));
+            Console.ReadKey();
+        }
+    }
+}
